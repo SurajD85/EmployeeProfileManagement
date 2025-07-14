8140c2901bee96e04ccc8667dfa6512283c1ff36
edocfifty <jarusdeuja@gmail.com>
2025-07-14 19:00:26 +0545

    Added ApplicationDbContext to EmployeeProfileManagement.Data project

---
+++ EmployeeProfileManagement.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeProfileManagement.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProfileManagement.Data.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
+        public DbSet<Invitation> Invitations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserCompany>()
+                .HasKey(uc => new { uc.UserId, uc.CompanyId });
+
+            modelBuilder.Entity<UserCompany>()
+                .HasOne(uc => uc.User)
+                .WithMany(u => u.UserCompanies)
+                .HasForeignKey(uc => uc.UserId);
+
+            modelBuilder.Entity<UserCompany>()
+                .HasOne(uc => uc.Company)
+                .WithMany(c => c.UserCompanies)
+                .HasForeignKey(uc => uc.CompanyId);
+        }
+    }
+}
